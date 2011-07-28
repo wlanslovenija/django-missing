d88dd3644287341adc5d211a998dd60340c40305
Mitar <mitar.git@tnode.com>
2011-07-28 22:58:03 +0200

    Added setup and readme files.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+
+setup(
+    name = 'django-missing',
+    version = VERSION,
+    description = 'Some missing features in Django. Are not missing anymore.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
+    author = 'Mitar',
+    author_email = 'mitar.django@tnode.com',
+    url = 'https://bitbucket.org/mitar/django-missing',
+    license = "GPLv3",
+    packages = find_packages(),
+    package_data = {
+        'missing' : [
+            'static/missing/*',
+        ],
+    },
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data = True,
+    zip_safe = False, 
+    install_requires = [
+        'django-staticfiles',
+    ],
+)
