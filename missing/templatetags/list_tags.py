5f61e187e182b59a4d5247afaf21f12d43b56894
Mitar <mitar.git@tnode.com>
2011-08-14 13:31:50 +0200

    Added list splitting filter.

---
+++ missing/templatetags/list_tags.py
@@ -0,0 +1,11 @@
+import math
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def split_list(value, size):
+    size = int(size)
+    for i in range(int(math.ceil(len(value) / float(size)))):
+        yield value[i*size:i*size+size]
