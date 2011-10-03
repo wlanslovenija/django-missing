0e281e2c7f31d0be5b47aa10e2eeaae5ebf9c8da
Mitar <mitar.git@tnode.com>
2011-10-03 15:11:06 +0200

    Added first test.

--- missing/tests.py
+++ missing/tests.py
@@ -1,23 +1,14 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+from django import template
+from django import test
 
-Replace these with more appropriate tests for your application.
-"""
+class ContextTagsTest(test.TestCase):
+    def test_setcontext_1(self):
+        with self.assertRaises(template.TemplateSyntaxError) as cm:
+            t = template.Template("""
+            {% load context_tags %}
+            {% setcontext %}
+            FooBar
+            {% endsetcontext %}
+            """)
 
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+        self.assertIn('expected format', str(cm.exception))
