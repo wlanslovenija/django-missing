eb70adeaaa2b4087cdc3ba93bf3eca4bd5fdbe39
Mitar <mitar.git@tnode.com>
2011-10-18 04:33:25 +0200

    Added `count` and `startswith` template filters.

--- missing/templatetags/string_tags.py
+++ missing/templatetags/string_tags.py
@@ -16,3 +16,19 @@ def ensure_sentence(value):
     if value and value[-1] not in ".?!,;)":
         return u"%s." % (value,)
     return value
+
+@register.filter
+def count(value, arg):
+    """
+    Returns the number of non-overlapping occurrences of an argument substring in the given string.
+    """
+
+    return value.count(arg)
+
+@register.filter
+def startswith(value, arg):
+    """
+    Returns True if the given string starts with an argument prefix, otherwise returns False.
+    """
+
+    return value.startswith(arg)
