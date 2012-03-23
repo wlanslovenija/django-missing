7e5413552c576811efab98ad1254e621613a0487
Mitar <mitar.git@tnode.com>
2012-03-23 13:46:47 +0100

    Show correct version in documentation. Version bump.

--- setup.py
+++ setup.py
@@ -2,35 +2,36 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
-setup(
-    name = 'django-missing',
-    version = VERSION,
-    description = 'Some missing features in Django. Are not missing anymore.',
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
-    author = 'Mitar',
-    author_email = 'mitar.django@tnode.com',
-    url = 'https://bitbucket.org/mitar/django-missing',
-    license = "AGPLv3",
-    packages = find_packages(),
-    package_data = {
-        'missing' : [
-            'static/missing/*',
+if __file__ == '__main__':
+    setup(
+        name = 'django-missing',
+        version = VERSION,
+        description = 'Some missing features in Django. Are not missing anymore.',
+        long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
+        author = 'Mitar',
+        author_email = 'mitar.django@tnode.com',
+        url = 'https://bitbucket.org/mitar/django-missing',
+        license = "AGPLv3",
+        packages = find_packages(),
+        package_data = {
+            'missing' : [
+                'static/missing/*',
+            ],
+        },
+        classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Affero General Public License v3',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Framework :: Django',
         ],
-    },
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-    ],
-    include_package_data = True,
-    zip_safe = False,
-    install_requires = [
-        'django-staticfiles',
-    ],
-)
+        include_package_data = True,
+        zip_safe = False,
+        install_requires = [
+            'django-staticfiles',
+        ],
+    )
