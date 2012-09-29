df2918b46abcd7306360c06f0826a1111db69817
Mitar <mitar.git@tnode.com>
2012-09-29 04:57:00 -0700

    Removed support for Django 1.2.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         include_package_data = True,
         zip_safe = False,
         install_requires = [
-            'Django>=1.2',
+            'Django>=1.3',
         ],
         test_suite = 'tests.runtests.runtests',
     )
