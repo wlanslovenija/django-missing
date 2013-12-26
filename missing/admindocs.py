f89db7b0b16fadab7d8dcbe66160187c48cfefe3
Mitar <mitar.git@tnode.com>
2013-12-26 11:05:20 +0100

    Don't use {} or [] as default function arguments.

--- missing/admindocs.py
+++ missing/admindocs.py
@@ -5,7 +5,11 @@ DUMMY_ROLES = (
     'py:func',
 )
 
-def dummy_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+def dummy_role(name, rawtext, text, lineno, inliner, options=None, content=None):
+    if options is None:
+        options = {}
+    if content is None:
+        content = []
     node = docutils.nodes.literal(rawtext, text, **options)
     return [node], []
 
