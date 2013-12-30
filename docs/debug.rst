94da12e17476a1e95e59ecf2619692da92492f22
Mitar <mitar.git@tnode.com>
2013-12-30 15:32:51 +0100

    Throw exceptions in templates on NoReverseMatch.

--- docs/debug.rst
+++ docs/debug.rst
@@ -1,5 +1,16 @@
 Debugging
 =========
 
+Safer ExceptionReporterFilter
+-----------------------------
+
 .. automodule:: missing.debug
     :members:
+
+Non-silent NoReverseMatch
+-------------------------
+
+``NoReverseMatch`` is by default a silent exception in variables, its output replaced by ``TEMPLATE_STRING_IF_INVALID``
+setting. Sometimes you want a bit more loud expression of mismatched URL reversing, so you can set
+``TEMPLATE_URL_RESOLVERS_DEBUG`` to ``True`` to normally raise an exception. Only active when ``TEMPLATE_DEBUG`` is
+set to ``True`` as well.
--- missing/models.py
+++ missing/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core import urlresolvers
 from django.template import loader
 
 # To load docutils extensions somewhere
@@ -5,3 +7,8 @@ from missing import admindocs
 
 # To be able to force use of "contextblock" tag immediately after "extends" tag, we add it to built-in tags
 loader.add_to_builtins('missing.templatetags.context_tags')
+
+# NoReverseMatch exceptions are silent (replaced by TEMPLATE_STRING_IF_INVALID setting), but we
+# disable this behavior here
+if getattr(settings, 'TEMPLATE_URL_RESOLVERS_DEBUG', False) and getattr(settings, 'TEMPLATE_DEBUG', False):
+    urlresolvers.NoReverseMatch.silent_variable_failure = False
