477d637bef7da9d6042b2ed94e605e43f6b09bfe
Jernej Kos <jernej@kos.mx>
2014-10-15 14:31:35 +0200

    add_to_builtins is in templates.base.
    This fixes a failure on Django 1.7 where add_to_builtins
    is not imported in templates.loader.

--- missing/models.py
+++ missing/models.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.core import urlresolvers
-from django.template import loader
+from django.template import base
 
 # To load docutils extensions somewhere
 from missing import admindocs
 
 # To be able to force use of "contextblock" tag immediately after "extends" tag, we add it to built-in tags
-loader.add_to_builtins('missing.templatetags.context_tags')
+base.add_to_builtins('missing.templatetags.context_tags')
 
 # NoReverseMatch exceptions are silent (replaced by TEMPLATE_STRING_IF_INVALID setting), but we
 # disable this behavior here
