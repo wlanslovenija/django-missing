a53c1be1c0747b088cd0472e8f37a2c3eac5bb47
Mitar <mitar.git@tnode.com>
2017-06-26 00:56:28 -0700

    Django 1.10.7 is also supported.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         include_package_data = True,
         zip_safe = False,
         install_requires = [
-            'Django>=1.11.2',
+            'Django>=1.10.7',
         ],
         test_suite = 'tests.runtests.runtests',
     )
